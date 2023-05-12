defc5d0fbb09409c87441c6d6f78084bbb4c1458
coka13 <isafeze@gmail.com>
2023-05-12 17:27:10 +0300

    Saving recordings on the db

--- Crybaby.py
+++ Crybaby.py
@@ -12,7 +12,8 @@ from PIL import Image
 from pymongo import MongoClient
 import subprocess
 from werkzeug.datastructures import FileStorage
-
+from datetime import datetime
+import soundfile as sf
 
 def predict(file=None):
     response = app.make_response(render_template('results.html')) 
@@ -160,23 +161,24 @@ else:
 
 # Define Recording schema
 class Recording:
-    def __init__(self, date, duration, filepath):
+    def __init__(self, name, date, duration, filedata, label=None, feedback=None):
+        self.name = name
         self.date = date
-        self.duration = duration
-        self.filepath = filepath
+        self.filedata = filedata
+        self.label = label
+        self.feedback = feedback
 
 # Define Newborn schema
 class Newborn:
-    def __init__(self, name, birthdate,gender):
+    def __init__(self, name, birthdate, gender):
         self.name = name
         self.birthdate = birthdate
-        self.gender=gender
+        self.gender = gender
         self.recordings = []
 
-    def add_recording(self, date, duration, filepath):
-        recording = Recording(date, duration, filepath)
+    def add_recording(self, name, date, duration, filedata, label=None, feedback=None):
+        recording = Recording(name, date, duration, filedata, label, feedback)
         self.recordings.append(recording)
-        
 
 
 # Define User schema
@@ -201,19 +203,6 @@ class User:
 
        
     
-    # EXAMPLES #
-    
-# user = User("username", "password", "email")
-# user.add_newborn("Baby", "2022-01-01")
-
-#newborn = user.get_newborn_by_name("Baby")
-
-#newborn.add_recording("2022-01-01", 60, "/path/to/recording.wav")
-
-
-################## DB ##################
-########################################
-
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -324,20 +313,34 @@ def upload():
     if 'logged_in' not in session or not session['logged_in']:
         # If not, redirect to the login page
         return redirect(url_for('login'))
-    
+    # get current user newborns
+    current_user = db.users.find_one({'username': session['username']})
+    newborns = current_user['newborns']
     if request.method == 'GET':
-        return render_template("upload.html")
+        return render_template("upload.html", newborns=newborns)
     else:
         file = request.files['file']
+        if file is None:
+            # Return an error message if the file is not in the request
+            return 'No file uploaded', 400
         # call the predict function to get the result
         res = predict(file)
         # pass the result to the template as a variable
         print(res)
-        return render_template("upload.html", result=res)
+        binary_data = file.read()
+        # Get the selected newborn's name from the form
+        selected_newborn_name = request.form['newborn_name']
+        # datetime object containing current date and time
+        now = datetime.now()
+        # dd/mm/YY H:M:S
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        recording_name = request.form['recording_name']
+        db.users.update_one(
+    {"username": session['username'], "newborns.name": selected_newborn_name},
+    {"$push": {"newborns.$.recordings": {"name": recording_name, "date": dt_string,  "file": binary_data, "label": res}}})
+        return render_template("upload.html", result=res,newborns=newborns)
 
 
-from scipy.io import wavfile
-import subprocess
 
 @app.route('/record', methods=['POST', 'GET'])
 def record():
@@ -345,8 +348,12 @@ def record():
     if 'logged_in' not in session or not session['logged_in']:
         # If not, redirect to the login page
         return redirect(url_for('login'))
-
-    if request.method == 'POST':
+    # get current user newborns
+    current_user = db.users.find_one({'username': session['username']})
+    newborns = current_user['newborns']
+    if request.method == 'GET':
+        return render_template("record.html", newborns=newborns)
+    else:
         # Get the file from the request
         file = request.files.get('file')
 
@@ -377,8 +384,6 @@ def record():
         print(result)
         return result
 
-    else:
-        return render_template("record.html")
 
 
 @app.route('/history')
